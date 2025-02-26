4462462b186840725663fde93c497f977b233f4b
J Snegha <snegha.j@centrico.tech>
2025-02-26 23:39:35 +0530

    Add workflow to test

--- scripts/test.py
+++ scripts/test.py
@@ -1,6 +1,23 @@
 
 import google.generativeai as genai
-def generate_script(topic):  
+import json
+from datetime import datetime
+def get_topic():
+    with open('files/topics.json', 'r') as file:
+        topics = json.load(file)
+    today = datetime.today().day
+    print(f"Today's date (day): {today}")
+    print(f"Topics available: {topics}") 
+    topic = topics.get(str(today))
+    if topic is None:
+        print("No topic found for today!")
+        exit(1)        
+    print(f"Selected topic: {topic}")
+    return  topic
+def generate_script(topic):
+
+
+    
     genai.configure(api_key="AIzaSyCU34NtFrX324iZbliAafvoUqOS-ZPnjiY")  
     model = genai.GenerativeModel("gemini-1.5-flash-latest")  
     response = model.generate_content(topic)  
@@ -19,9 +36,11 @@ def generate_script(topic):
     return con  
 
 print("testing")
-topic = """Generate a high-engagement LinkedIn post (100-150 words) on Lessons I’ve learned in my developer journey so far in a professional yet conversational tone for a junior developer in AI, IT, or software. Start with a bold statement, thought-provoking question, or personal insight to grab attention. Provide concise, valuable insights with actionable takeaways, avoiding generic statements. Ensure a smooth flow without unnecessary spaces. Use emojis naturally to highlight key points and improve readability without overuse. End with a compelling thought that sparks discussion and ask a question to invite comments. Add 4-7 relevant LinkedIn hashtags for maximum reach, ensuring they are popular yet specific"""
+topic=get_topic()
 print(topic)
-con=generate_script(topic)
+prompt = """Generate a high-engagement LinkedIn post (100-150 words) on {topic} so far in a professional yet conversational tone for a junior developer in AI, IT, or software. Start with a bold statement, thought-provoking question, or personal insight to grab attention. Provide concise, valuable insights with actionable takeaways, avoiding generic statements. Ensure a smooth flow without unnecessary spaces. Use emojis naturally to highlight key points and improve readability without overuse. End with a compelling thought that sparks discussion and ask a question to invite comments. Add 4-7 relevant LinkedIn hashtags for maximum reach, ensuring they are popular yet specific"""
+print(prompt)
+con=generate_script(prompt)
 print("*********")
 print(con)
 
